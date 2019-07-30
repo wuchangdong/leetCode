343ecae4df2eb904109c9aabc9624cbc1e8a71ee
wuchangdong <1737149371@qq.com>
2019-07-30 11:35:19 +0800

    remove-duplicates-from-sorted-array

--- 0026删除排序数组中的重复项/index.js
+++ 0026删除排序数组中的重复项/index.js
@@ -7,6 +7,22 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+// 双指针法
+var removeDuplicates = function (nums) {
+    if (nums.length === 0) {
+        return 0;
+    }
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+};
+
 var removeDuplicates = function (nums) {
     for (let i = nums.length - 1; i >= 0; i--) {
         if (i === 0) {
