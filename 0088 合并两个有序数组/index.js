4713ea6452bea6b6f9d86775bac0aedaad54e64f
wuchangdong <1737149371@qq.com>
2019-08-02 16:18:55 +0800

    merge-sorted-array

---
+++ 0088 合并两个有序数组/index.js
@@ -0,0 +1,23 @@
+/**
+ * @file merge-sorted-array
+ * @author wuchangdong
+ */
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+var merge = function (nums1, m, nums2, n) {
+    let len1 = m - 1;
+    let len2 = n - 1;
+    let len = m + n - 1;
+    while (len1 >= 0 && len2 >= 0) {
+        nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
+    }
+    nums1.splice(0, len2 + 1, ...nums2.slice(0, len2 + 1));
+    return nums1;
+};
