07050847a8153eff1f5c942693b5611b5c9c2264
wuchangdong <1737149371@qq.com>
2019-08-09 11:26:32 +0800

    pascals-triang-li

---
+++ 0119杨辉三角 II/index.js
@@ -0,0 +1,22 @@
+/**
+ * @file pascals-triangle-ii
+ * @author wuchangdong
+ */
+
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+
+var getRow = function (rowIndex) {
+    let lastRow = [1];
+    for (let i = 1; i < rowIndex + 1; i++) {
+        const newRow = [1];
+        for (let j = 0; j < i - 1; i++) {
+            newRow.push(lastRow[j] + lastRow[j + 1]);
+        }
+        newRow.push(1);
+        lastRow = newRow;
+    }
+    return lastRow;
+};
