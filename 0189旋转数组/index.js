e6859690b727952759094bf12cd02b408f4e8eac
wuchangdong <1737149371@qq.com>
2019-08-19 16:05:12 +0800

    rotate-array

---
+++ 0189旋转数组/index.js
@@ -0,0 +1,20 @@
+/**
+ * @file factorial-trailing-zeroes
+ * @author wuchangdong
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+var rotate = function (nums, k) {
+    if (k >= nums.length) {
+        k %= nums.length;
+    }
+    for (let i = 0; i < k; i++) {
+        nums.unshift(nums.pop(nums[nums.length - 1]));
+    }
+    return nums;
+};
