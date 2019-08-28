e4b0c8fbec1f0c3763cf6e865a708441b80b18bc
v-wuchangdong <v_wuchangdong@baidu.com>
2019-08-28 11:08:29 +0800

    power-of-two

---
+++ 0231 2的幂/index.js
@@ -0,0 +1,16 @@
+/**
+ * @file power-of-two
+ * @author wuchangdong
+ */
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+var isPowerOfTwo = function (n) {
+    if (n < 1) {
+        return false;
+    }
+    return (n & (n - 1)) === 0;
+};
