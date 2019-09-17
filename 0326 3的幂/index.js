f74de786850fe70ffe3bff22b8e2c338b1a751f8
v-wuchangdong <v_wuchangdong@baidu.com>
2019-09-17 15:38:38 +0800

    power-of-three

---
+++ 0326 3的幂/index.js
@@ -0,0 +1,19 @@
+/**
+ * @file power-of-three
+ * @author wuchangdong
+ */
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+var isPowerOfThree = function (n) {
+    if (n < 1) {
+        return false;
+    }
+    while (n % 3 === 0) {
+        n /= 3;
+    }
+    return n === 1;
+};
