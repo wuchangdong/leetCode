a3363689d92e1050312de0a91b643fa082c15bdb
v-wuchangdong <v_wuchangdong@baidu.com>
2019-09-24 15:54:39 +0800

    find-the-difference

---
+++ 0389找不同/index.js
@@ -0,0 +1,22 @@
+/**
+ * @file first-unique-character-in-a-string
+ * @author wuchangdong
+ */
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+
+var findTheDifference = function (s, t) {
+    let code = 0;
+    //  a ^ b ^ b = a ^ 0 = a
+    // 通过位运算消除相同元素
+    for (let i = 0; i < s.length; i++) {
+        code ^= s.charCodeAt(i);
+        code ^= t.charCodeAt(i);
+    }
+    code ^= t.charCodeAt(t.length - 1);
+    return String.fromCharCode(code);
+};
