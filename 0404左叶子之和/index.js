ef0afa61468c2293f6022743398c9f9f41ba8200
v-wuchangdong <v_wuchangdong@baidu.com>
2019-09-25 15:33:58 +0800

    sum-of-left-leaves

---
+++ 0404左叶子之和/index.js
@@ -0,0 +1,33 @@
+/**
+ * @file sum-of-left-leaves
+ * @author wuchangdong
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var sumOfLeftLeaves = function (root) {
+    if (!root) {
+        return 0;
+    }
+    let sum = 0;
+    function inner(node, flag) {
+        if (!node.left && !node.right && flag) {
+            sum += node.val;
+            return;
+        }
+        node.left && inner(node.left, true);
+        node.right && inner(node.right, false);
+    }
+    inner(root, false);
+    return sum;
+};
