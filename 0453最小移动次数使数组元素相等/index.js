4a9691e1369b52fee541d5723de7b2ba9cd8a2d4
v-wuchangdong <v_wuchangdong@baidu.com>
2019-10-15 16:38:18 +0800

    minimum-moves-to-equal-array-elements

---
+++ 0453最小移动次数使数组元素相等/index.js
@@ -0,0 +1,42 @@
+/**
+ * @file minimum-moves-to-equal-array-elements
+ * @author wuchangdong
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+
+// 对n - 1个数 + 1  就是对剩余那个数减一 都减到最小数就能得到需要多少步
+var minMoves = function (nums) {
+    let sum = 0;
+    let min = Infinity;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        min = Math.min(min, nums[i]);
+    }
+    return sum - min * nums.length;
+};
+
+// 动态规划
+var minMoves = function (nums) {
+    nums = nums.sort((a, b) => {
+        if (a < b) {
+            return -1;
+        }
+        else if (a > b) {
+            return 1;
+        }
+        return 0;
+    });
+
+    let moves = 0;
+    for (let i = 1; i < nums.length; i++) {
+        const diff = moves + nums[i] - nums[i - 1];
+        nums[i] += moves;
+        moves += diff;
+    }
+    return moves;
+};
