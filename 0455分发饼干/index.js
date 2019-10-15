b8f4804666755c3546767d694b1a11c57c1b8ef5
v-wuchangdong <v_wuchangdong@baidu.com>
2019-10-15 17:23:42 +0800

    assign-cookies

--- 0455分发饼干/index.js
+++ 0455分发饼干/index.js
@@ -10,5 +10,17 @@
  */
 
 var findContentChildren = function (g, s) {
-    
-}
+    g = g.sort((a, b) => a - b);
+    s = s.sort((a, b) => a - b);
+    let count = 0;
+    let startIndex = 0;
+    for (let i = 0; i < g.length; i++) {
+        while (startIndex < s.length) {
+            if (s[startIndex++] >= g[i]) {
+                count++;
+                break;
+            }
+        }
+    }
+    return count;
+};
