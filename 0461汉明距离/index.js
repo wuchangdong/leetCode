1e9f2456116c4c5aa46048dab4bc690e02409a0f
v-wuchangdong <v_wuchangdong@baidu.com>
2019-10-17 16:17:11 +0800

    hamming-distance

---
+++ 0461汉明距离/index.js
@@ -0,0 +1,22 @@
+/**
+ * @file hamming-distance
+ * @author wuchangdong
+ */
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+
+var hammingDistance = function (x, y) {
+    let count = 0;
+    while (x > 0 || y > 0) {
+        if ((x & 1) ^ (y & 1) === 1) {
+            count++;
+        }
+        x >>= 1;
+        y >>= 1;
+    }
+    return count;
+};
